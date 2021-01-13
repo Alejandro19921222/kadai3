40226a07a92e74e6ec97bfe7eb2c03bb0a6769b8
Alejandro19921222 <niojapon@gmail.com>
2021-01-13 10:33:59 +0000

    [day1finish]kadai3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  sessions: 'devise/sessions',
+  registrations: 'devise/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: "homes#top"
+get "about", to: "homes#about"
+
 end
